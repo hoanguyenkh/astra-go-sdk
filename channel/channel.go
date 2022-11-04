b70fa1cd0ba1fc634785c95ed7836d78bfbc6846
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-11-04 11:39:11 +0700

    add done open channel

--- channel/channel.go
+++ channel/channel.go
@@ -7,6 +7,8 @@ import (
 	"github.com/AstraProtocol/astra-go-sdk/common"
 	channelTypes "github.com/AstraProtocol/channel/x/channel/types"
 	"github.com/cosmos/cosmos-sdk/client"
+	cryptoTypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/pkg/errors"
 )
 
 type Channel struct {
@@ -17,24 +19,29 @@ func NewChannel(rpcClient client.Context) *Channel {
 	return &Channel{rpcClient}
 }
 
-func (cn *Channel) OpenChannel(request OpenChannelRequest) (string, error) {
+func (cn *Channel) SignOpenChannel(request OpenChannelRequest,
+	account *account.PrivateKeySerialized,
+	multiSigPubkey cryptoTypes.PubKey) (string, error) {
 	msg := channelTypes.NewMsgOpenChannel(request.Creator, request.PartA, request.PartB,
 		request.CoinA, request.CoinB, request.MultisigAddr, request.Sequence)
 	err := msg.ValidateBasic()
 	if err != nil {
 		return "", err
 	}
-	tmpPrivKey := account.PrivateKeySerialized{}
-	newTx := common.NewTx(cn.rpcClient,
-		account.NewPrivateKeySerialized("", tmpPrivKey.PrivateKey()), 0, "")
+	newTx := common.NewTxMulSign(cn.rpcClient, account, request.GasLimit, request.GasPrice, 0, 2)
 	txBuilder, err := newTx.BuildUnsignedTx(msg)
 
-	json, err := cn.rpcClient.TxConfig.TxJSONEncoder()(txBuilder.GetTx())
+	err = newTx.SignTxWithSignerAddress(txBuilder, multiSigPubkey)
+	if err != nil {
+		return "", errors.Wrap(err, "SignTx")
+	}
+
+	sign, err := common.TxBuilderSignatureJsonEncoder(cn.rpcClient.TxConfig, txBuilder)
 	if err != nil {
 		return "", err
 	}
-
-	return fmt.Sprintf("%s\n", json), nil
+	fmt.Println("sign-data", string(sign))
+	return sign, nil
 }
 
 func (cn *Channel) ListChannel() (*channelTypes.QueryAllChannelResponse, error) {
